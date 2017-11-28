6cc27b3020bee563a8b13dd711a60f9782bcfc47
Adam Okoń <adam.okon@elegantcode.pl>
2017-11-28 23:11:49 +0100

    islands_engine: add basic Guesses.new function

--- islands_engine/lib/islands_engine/guesses.ex
+++ islands_engine/lib/islands_engine/guesses.ex
@@ -1,4 +1,10 @@
 defmodule IslandsEngine.Guesses do
+  alias __MODULE__
+
   @enforce_keys [:hits, :misses]
   defstruct [:hits, :misses]
+
+  def new do
+    {:ok, %Guesses{hits: MapSet.new, misses: MapSet.new}}
+  end
 end
