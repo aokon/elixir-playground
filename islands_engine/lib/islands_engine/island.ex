ab7f8f53593dd38275c24be66ff7d1f48ea12276
Adam Okoń <adam.okon@elegantcode.pl>
2017-12-13 23:21:40 +0100

    islands_engine: add fallback for offsets

--- islands_engine/lib/islands_engine/island.ex
+++ islands_engine/lib/islands_engine/island.ex
@@ -11,4 +11,5 @@ defmodule IslandsEngine.Island do
   defp offests(:dot), do: [{0,0}]
   defp offsets(:l_shape), do: [{0,0}, {1,0}, {2,0}, {2,1}]
   defp offsets(:s_shape), do: [{0,1}, {0,2}, {1,0}, {1,1}]
+  defp offsets(_), do: {:error, :invalid_island_type}
 end
