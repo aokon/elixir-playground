a7a20bb532ffb8bb35d3c0080117ffba4e4f9373
Adam Okoń <adam.okon@elegantcode.pl>
2017-12-29 16:40:07 +0100

    islands_engine: provide add_coordinates utils for IslandsEngine.Island

--- islands_engine/lib/islands_engine/island.ex
+++ islands_engine/lib/islands_engine/island.ex
@@ -1,5 +1,6 @@
 defmodule IslandsEngine.Island do
   alias __MODULE__
+  alias IslandsEngine.Coordinate
 
   @enforce_keys [:coordinates, :hit_coordinates]
   defstruct [:coordinates, :hit_coordinates]
@@ -12,4 +13,19 @@ defmodule IslandsEngine.Island do
   defp offsets(:l_shape), do: [{0,0}, {1,0}, {2,0}, {2,1}]
   defp offsets(:s_shape), do: [{0,1}, {0,2}, {1,0}, {1,1}]
   defp offsets(_), do: {:error, :invalid_island_type}
+
+  defp add_cooridinates(offsets, upper_left) do
+    Enum.reduce_while(offsets, MapSet.new(), fn offset, acc ->
+      add_coordinate(acc, upper_left, offset)
+    end)
+  end
+
+  defp add_coordinate(coordinates, %Coordinate{ row: row, col: col }, {row_offset, col_offset}) do
+    case Coordinate.new(row + row_offset, col + col_offset) do
+      {:ok, coordinate} ->
+        {:cont, MapSet.put(coordinates, coordinate)}
+      {:error, :invalid_coordinate} ->
+        {:halt, {:error, :invalid_coordinate}}
+    end
+  end
 end
