9b848106662ea63ae3b6ddf5e1fe6f6cfa4964be
Adam Okoń <adam.okon@elegantcode.pl>
2018-02-27 22:30:13 +0100

    islands_engine: add new version for Island.new

--- islands_engine/lib/islands_engine/island.ex
+++ islands_engine/lib/islands_engine/island.ex
@@ -5,11 +5,19 @@ defmodule IslandsEngine.Island do
   @enforce_keys [:coordinates, :hit_coordinates]
   defstruct [:coordinates, :hit_coordinates]
 
-  def new, do: %Island{coordinates: MapSet.new, hit_coordinates: MapSet.new}
+  def new(type, %Coordinate{} = upper_left) do
+    with [_|_] = offsets <- offsets(type),
+         %MapSet{} = coordinates <- add_cooridinates(offsets, upper_left)
+    do
+      {:ok, %Island{coordinates: coordinates, hit_coordinates: MapSet.new()}}
+    else
+      error -> error
+    end
+  end
 
-  defp offests(:square), do: [{0,0}, {0,1}, {1,0}, {1,1}]
-  defp offests(:atol), do: [{0,0}, {0,1}, {1,1}, {2,1}, {2,2}]
-  defp offests(:dot), do: [{0,0}]
+  defp offsets(:square), do: [{0,0}, {0,1}, {1,0}, {1,1}]
+  defp offsets(:atol), do: [{0,0}, {0,1}, {1,1}, {2,1}, {2,2}]
+  defp offsets(:dot), do: [{0,0}]
   defp offsets(:l_shape), do: [{0,0}, {1,0}, {2,0}, {2,1}]
   defp offsets(:s_shape), do: [{0,1}, {0,2}, {1,0}, {1,1}]
   defp offsets(_), do: {:error, :invalid_island_type}
