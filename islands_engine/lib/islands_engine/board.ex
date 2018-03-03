060d1cc45a8fb37be1810183be966e25ff8b999c
Adam Okoń <adam.okon@elegantcode.pl>
2018-03-03 12:56:59 +0100

    islands_engine: add utils functions related to islands for the Board module

--- islands_engine/lib/islands_engine/board.ex
+++ islands_engine/lib/islands_engine/board.ex
@@ -1,3 +1,22 @@
 defmodule IslandsEngine.Board do
+  alias IslandsEngine.Island
+
   def new(), do: %{}
+
+  def position_island(board, key, %Island{} = island) do
+    case overlap_existing_island?(board, key, island) do
+      true -> {:error, :overlapping_island}
+      false -> Map.put(board, key, island)
+    end
+  end
+
+  def all_islands_positioned?(board) do
+    Enum.all?(Island.types, &(Map.has_key?(board, &1)))
+  end
+
+  defp overlap_existing_island?(board, new_key, new_island) do
+    Enum.any?(board, fn {key, island} ->
+       key != new_key and Island.overlap?(island, new_island)
+    end)
+  end
 end
