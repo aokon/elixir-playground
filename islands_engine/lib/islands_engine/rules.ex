d6eae6e9ecd12a5c085ddc258cd3a6cbe3b56f6e
Adam Okoń <adam.okon@elegantcode.pl>
2018-03-06 22:23:48 +0100

    islands_engine: add rules to state machine for position_islands

--- islands_engine/lib/islands_engine/rules.ex
+++ islands_engine/lib/islands_engine/rules.ex
@@ -1,12 +1,21 @@
 defmodule IslandsEngine.Rules do
   alias __MODULE__
 
-  defstruct state: :initialized
+  defstruct state: :initialized,
+            player1: :islands_not_set,
+            player2: :islands_not_set
 
   def new(), do: %Rules{}
 
-  def check(%Rules{state: :initialized} = state, :add_player),
-    do: {:ok, %Rules{state | state: :players_set}}
+  def check(%Rules{state: :initialized} = rules, :add_player),
+    do: {:ok, %Rules{rules | state: :players_set}}
+
+  def check(%Rules{state: :players_set} = rules, {:position_islands, player}) do
+    case Map.fetch!(rules, player) do
+      :islands_set -> :error
+      :islands_not_set -> {:ok, rules}
+    end
+  end
 
   def check(_state, _action), do: :error
 end
