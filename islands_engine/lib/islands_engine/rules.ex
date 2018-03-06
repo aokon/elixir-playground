8c9304c935b56cb92ed2b9591efb2e021b759b85
Adam Okoń <adam.okon@elegantcode.pl>
2018-03-06 22:03:45 +0100

    islands_engine: add trasition from initialized to players_set state

--- islands_engine/lib/islands_engine/rules.ex
+++ islands_engine/lib/islands_engine/rules.ex
@@ -5,5 +5,8 @@ defmodule IslandsEngine.Rules do
 
   def new(), do: %Rules{}
 
+  def check(%Rules{state: :initialized} = state, :add_player),
+    do: {:ok, %Rules{state | state: :players_set}}
+
   def check(_state, _action), do: :error
 end
