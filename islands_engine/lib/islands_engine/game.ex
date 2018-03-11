e2972ad533c55fbcc103a5e56d4e7e620c47163f
Adam Okoń <adam.okon@elegantcode.pl>
2018-03-11 11:46:35 +0100

    islands_engine: add handling add_player to Game

--- islands_engine/lib/islands_engine/game.ex
+++ islands_engine/lib/islands_engine/game.ex
@@ -11,4 +11,27 @@ defmodule IslandsEngine.Game do
     player2 = %{name: nil, board: Board.new(), guesses: Guesses.new()}
     {:ok, %{player1: player1, player2: player2, rules: %Rules{}}}
   end
+
+  def add_player(game, name) when is_binary(name), do:
+    GenServer.call(game, {:add_player, name})
+
+  def handle_call({:add_player, name}, _from, state_data) do
+    with {:ok, rules} <- Rules.check(state_data.rules, :add_player) do
+      state_data
+      |> update_player2_name(name)
+      |> update_rules(rules)
+      |> reply_success(:ok)
+    else
+      :error -> {:reply, :error, state_data}
+    end
+  end
+
+  defp update_player2_name(state_data, name), do:
+    put_in(state_data.player2.name, name)
+
+  defp update_rules(state_data, rules), do:
+    %{state_data | rules: rules}
+
+  defp reply_success(state_data, reply), do:
+    {:reply, reply, state_data}
 end
