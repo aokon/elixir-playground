af13069c71d1bf3a71cfd04b0dc32af0adda554b
Adam Okoń <adam.okon@elegantcode.pl>
2018-03-19 12:27:30 +0100

    islands_engine: add start_game for GameSupervisor

--- islands_engine/lib/islands_engine/game_supervisor.ex
+++ islands_engine/lib/islands_engine/game_supervisor.ex
@@ -6,5 +6,9 @@ defmodule IslandsEngine.GameSupervisor do
   def start_link(_options),
     do: Supervisor.start_link(__MODULE__, :ok, name: __MODULE__)
 
-  def init(:ok), do: Supervisor.init([Game], strategy: :simple_one_for_one)
+  def init(:ok),
+    do: Supervisor.init([Game], strategy: :simple_one_for_one)
+
+  def start_game(name),
+    do: Supervisor.start_child(__MODULE__, [name])
 end
