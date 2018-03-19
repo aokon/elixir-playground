641fa5f29d11a7e31087a5f35f7bc5d11c023c6b
Adam Okoń <adam.okon@elegantcode.pl>
2018-03-19 15:10:50 +0100

    islands_engine: cleanup game data during stop_game by the GameSupervisor

--- islands_engine/lib/islands_engine/game_supervisor.ex
+++ islands_engine/lib/islands_engine/game_supervisor.ex
@@ -12,8 +12,10 @@ defmodule IslandsEngine.GameSupervisor do
   def start_game(name),
     do: Supervisor.start_child(__MODULE__, [name])
 
-  def stop_game(name),
-    do: Supervisor.terminate_child(__MODULE__, pid_from_name(name))
+  def stop_game(name) do
+    :ets.delete(:game_state, name)
+    Supervisor.terminate_child(__MODULE__, pid_from_name(name))
+  end
 
   defp pid_from_name(name) do
     name
