ecb300ac8ccabbd12d438be2dc648636c97b5126
Adam Okoń <adam.okon@elegantcode.pl>
2018-03-19 14:28:59 +0100

    islands_engine: create game state table during app startup

--- islands_engine/lib/islands_engine/application.ex
+++ islands_engine/lib/islands_engine/application.ex
@@ -14,6 +14,8 @@ defmodule IslandsEngine.Application do
       IslandsEngine.GameSupervisor
     ]
 
+    :ets.new(:game_state, [:public, :named_table])
+
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: IslandsEngine.Supervisor]
