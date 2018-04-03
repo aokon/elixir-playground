2b1d1c945518282593173b2b4a4ad261b08a7749
Adam Okoń <adam.okon@elegantcode.pl>
2018-04-03 22:24:39 +0200

    islands_interface: add handlers for GameChannel

--- islands_interface/lib/islands_interface_web/channels/game_channel.ex
+++ islands_interface/lib/islands_interface_web/channels/game_channel.ex
@@ -6,4 +6,22 @@ defmodule IslandsInterfaceWeb.GameChannel do
   def join("game:" <> _player, _payload, socket) do
     {:ok, socket}
   end
+
+  # Playground with channels \o/
+
+  def handle_in("hello", payload, socket) do
+    # payload = %{message: "We forced this error"}
+    {:reply, {:ok, payload}, socket}
+    # {:reply, {:error, payload}, socket}
+  end
+
+  def handle_in("event", payload, socket) do
+    push socket, "emits_event", payload
+    {:noreply, socket}
+  end
+
+  def handle_in("broadcast", payload, socket) do
+    broadcast! socket, "emits_event", payload
+    {:noreply, socket}
+  end
 end
