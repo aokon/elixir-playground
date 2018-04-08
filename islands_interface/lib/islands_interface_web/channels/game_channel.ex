d5ea1ffb6154ba85f43e88ceebc2027ff91e76bf
Adam Okoń <adam.okon@elegantcode.pl>
2018-04-08 19:41:11 +0200

    islands_interface: add basic authorization during joing to the game

--- islands_interface/lib/islands_interface_web/channels/game_channel.ex
+++ islands_interface/lib/islands_interface_web/channels/game_channel.ex
@@ -5,8 +5,12 @@ defmodule IslandsInterfaceWeb.GameChannel do
   alias IslandsInterfaceWeb.Presence
 
   def join("game:" <> _player, %{"screen_name" => screen_name}, socket) do
-    send(self(), {:after_join, screen_name})
-    {:ok, socket}
+    if authorized?(socket, screen_name) do
+      send(self(), {:after_join, screen_name})
+      {:ok, socket}
+    else
+      {:error, %{reason: "unauthorized"}}
+    end
   end
 
   def handle_info({:after_join, screen_name}, socket) do
@@ -104,4 +108,21 @@ defmodule IslandsInterfaceWeb.GameChannel do
   end
 
   defp via("game:" <> player), do: Game.via_tupple(player)
+
+  defp number_of_players(socket) do
+    socket
+    |> Presence.list()
+    |> Map.keys()
+    |> length()
+  end
+
+  defp existing_player?(socket, screen_name) do
+    socket
+    |> Presence.list()
+    |> Map.has_key?(screen_name)
+  end
+
+  defp authorized?(socket, screen_name) do
+    number_of_players(socket) < 2 && !existing_player?(socket, screen_name)
+  end
 end
