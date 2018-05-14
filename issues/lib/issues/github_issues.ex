799517770d0e0647327a1f65627f3f446aa1f4f1
Adam Okoń <adam.okon@elegantcode.pl>
2018-05-14 18:36:02 +0200

    issues - add fetching issues for user project

--- issues/lib/issues/github_issues.ex
+++ issues/lib/issues/github_issues.ex
@@ -1,5 +1,13 @@
 defmodule Issues.GithubIssues do
+  @user_agent [ {"User-agent", "Elixir"} ]
+  @api_url"https://api.github.com"
+
   def fetch(user, project) do
-    IO.puts "Fetching #{user} #{project} data...."
+    project_issues_url(user, project)
+    |> HTTPoison.get(@user_agent)
+    |> IO.inspect()
   end
+
+  defp project_issues_url(user, project),
+    do: "#{@api_url}/repos/#{user}/#{project}/issues"
 end
