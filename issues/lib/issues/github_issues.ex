bfddf1e3d1beaa62d64b9a9a4277b14eccd9bf6e
Adam Okoń <adam.okon@elegantcode.pl>
2018-05-15 17:25:10 +0200

    issues - add handling github response

--- issues/lib/issues/github_issues.ex
+++ issues/lib/issues/github_issues.ex
@@ -1,13 +1,23 @@
 defmodule Issues.GithubIssues do
-  @user_agent [ {"User-agent", "Elixir"} ]
-  @api_url"https://api.github.com"
+  @user_agent [ {"User-agent", "Elixir learning"} ]
+  @github_url "https://api.github.com"
 
   def fetch(user, project) do
     project_issues_url(user, project)
     |> HTTPoison.get(@user_agent)
-    |> IO.inspect()
+    |> handle_response()
+  end
+
+  def handle_response({_, %{status_code: status_code, body: body}}) do
+    {
+      status_code |> check_for_status(),
+      body |> Poison.Parser.parse!()
+    }
   end
 
   defp project_issues_url(user, project),
-    do: "#{@api_url}/repos/#{user}/#{project}/issues"
+    do: "#{@github_url}/repos/#{user}/#{project}/issues"
+
+  defp check_for_status(200), do: :ok
+  defp check_for_status(_), do: :error
 end
