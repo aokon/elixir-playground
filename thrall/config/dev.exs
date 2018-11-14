b08bfb48cb5eeb9e57e5e3bc770766adc1218401
Adam Okoń <adam.okon@elegantcode.pl>
2018-11-14 19:24:58 +0100

    thrall - add loading confing for dev db via env vars

--- thrall/config/dev.exs
+++ thrall/config/dev.exs
@@ -68,8 +68,8 @@ config :phoenix, :plug_init_mode, :runtime
 
 # Configure your database
 config :thrall, Thrall.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: System.get_env("POSTGRESQL_USERNAME"),
+  password: System.get_env("POSTGRESQL_PASSWORD"),
   database: "thrall_dev",
   hostname: "localhost",
   pool_size: 10
