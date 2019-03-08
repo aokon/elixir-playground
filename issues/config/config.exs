2e0edf16e560c12013fcb112436925e680a87b5b
Adam Okoń <adam.okon@elegantcode.pl>
2019-03-08 23:58:11 +0100

    issues: add sample usage of the logger

--- issues/config/config.exs
+++ issues/config/config.exs
@@ -3,3 +3,5 @@
 use Mix.Config
 
 config :issues, github_url: "https://api.github.com"
+
+config :logger, compile_time_purge_level: :info
--- issues/lib/issues/github_issues.ex
+++ issues/lib/issues/github_issues.ex
@@ -2,13 +2,17 @@ defmodule Issues.GithubIssues do
   @user_agent [{"User-agent", "Elixir learning"}]
   @github_url Application.get_env(:issues, :github_url)
 
+  require Logger
+
   def fetch(user, project) do
+    Logger.info("Fetching #{user}'s project #{project}...")
     project_issues_url(user, project)
     |> HTTPoison.get(@user_agent)
     |> handle_response()
   end
 
   def handle_response({_, %{status_code: status_code, body: body}}) do
+    Logger.info("Got response: status code=#{status_code}")
     {
       status_code |> check_for_status(),
       body |> Poison.Parser.parse!()
