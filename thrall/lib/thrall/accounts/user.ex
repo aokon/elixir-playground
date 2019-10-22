5ec640b9a475c75d8c828ed4bea4e77c1aab18ae
Adam Okoń <adam.okon@elegantcode.pl>
2019-10-22 22:17:04 +0200

    thrall: add user for Accounts context

---
+++ thrall/lib/thrall/accounts/user.ex
@@ -0,0 +1,21 @@
+defmodule Thrall.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :email, :string
+    field :name, :string
+
+    timestamps()
+  end
+
+  @required_fields ~w(name)a
+  @optional_fields ~w(email)a
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, @required_fields ++ @optional_fields)
+    |> validate_required(@required_fields)
+  end
+end
