4571d14f5dfa61213b8a1632370c9958cde80a34
Adam Okoń <adam.okon@elegantcode.pl>
2019-12-15 21:53:22 +0100

    servy: move tests for handler to temp function

--- servy/lib/servy.ex
+++ servy/lib/servy.ex
@@ -1,2 +1,40 @@
 defmodule Servy do
+  def call do
+    request = """
+    GET /wildfire HTTP/1.1
+    Host: example.com
+    User-Agent: ExampleBrowser/1.0
+    Accept: */*
+
+    """
+
+    IO.inspect Servy.Handler.handle(request)
+
+    request = """
+    GET /bears HTTP/1.1
+    Host: example.com
+    User-Agent: ExampleBrowser/1.0
+    Accept: */*
+
+    """
+    IO.inspect Servy.Handler.handle(request)
+
+    request = """
+    DELETE /bears/22 HTTP/1.1
+    Host: example.com
+    User-Agent: ExampleBrowser/1.0
+    Accept: */*
+
+    """
+    IO.inspect Servy.Handler.handle(request)
+
+    request = """
+    GET /bigfoot HTTP/1.1
+    Host: example.com
+    User-Agent: ExampleBrowser/1.0
+    Accept: */*
+
+    """
+    IO.inspect Servy.Handler.handle(request)
+  end
 end
