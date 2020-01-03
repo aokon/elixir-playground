16c4d308349cb7fc61767c12e259f0af5cbbe708
Adam Okoń <adam.okon@elegantcode.pl>
2020-01-03 19:16:30 +0100

    servy: add parsing the HTTP headers

--- servy/lib/servy/parser.ex
+++ servy/lib/servy/parser.ex
@@ -7,15 +7,25 @@ defmodule Servy.Parser do
 
     [method, path, _] = String.split(request_lines, " ")
     params = parse_params(params_string)
+    headers = parse_headers(header_lines, %{})
 
     %Conv{
       method: method,
       path: path,
-      params: params
+      params: params,
+      headers: headers
     }
   end
 
   def parse_params(params_string) do
     params_string |> String.trim |> URI.decode_query
   end
+
+  def parse_headers([head | tail], headers) do
+    [key, value] = String.split(head, ": ")
+    headers = Map.put(headers, key, value)
+    parse_headers(tail, headers)
+  end
+
+  def parse_headers([], headers), do: headers
 end
