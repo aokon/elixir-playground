e2beb52633c1f58718f0dd27e2d8d3872e0dc5d8
Adam Okoń <adam.okon@elegantcode.pl>
2020-01-16 21:44:24 +0100

    servy: add error handler for HTTPClient

--- servy/lib/servy/http_client.ex
+++ servy/lib/servy/http_client.ex
@@ -3,23 +3,27 @@ defmodule Servy.HTTPClient do
   @port 4000
 
   def get(request) do
-    with client_sock = connect(),
-         :ok = :gen_tcp.send(client_sock, request),
-         response = get_response(client_sock),
-    do: {:ok, response}
+    with {:ok, client_sock} <- connect(),
+         :ok <- :gen_tcp.send(client_sock, request),
+         {:ok, response} <- get_response(client_sock)
+    do
+      {:ok, response}
+    else
+      error -> error
+    end
   end
 
   def connect() do
-    {:ok, client_sock} = :gen_tcp.connect(@host, @port,
-                                 [:binary, packet: :raw, active: false])
-
-    client_sock
+    :gen_tcp.connect(@host, @port, [:binary, packet: :raw, active: false])
   end
 
   def get_response(client_sock) do
-    {:ok, response} = :gen_tcp.recv(client_sock, 0)
-    :ok = :gen_tcp.close(client_sock)
-
-    response
+    with {:ok, response} <- :gen_tcp.recv(client_sock, 0),
+         :ok <- :gen_tcp.close(client_sock)
+    do
+      {:ok, response}
+    else
+      error -> error
+    end
   end
 end
