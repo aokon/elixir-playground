dd6e254b3763abfde3485fa59014866001bfb1ae
Adam Okoń <adam.okon@elegantcode.pl>
2020-01-23 22:20:30 +0100

    servy: make refactor for HTTPClient

--- servy/lib/servy/http_client.ex
+++ servy/lib/servy/http_client.ex
@@ -1,29 +1,39 @@
 defmodule Servy.HTTPClient do
-  @host 'localhost'
-  @port 4000
+  def get(host, port, path) when is_binary(host) do
+    host_as_charlist = host |> String.to_charlist()
+    get(host_as_charlist, port, path)
+  end
 
-  def get(request) do
-    with {:ok, client_sock} <- connect(),
-         :ok <- :gen_tcp.send(client_sock, request),
-         {:ok, response} <- get_response(client_sock)
-    do
+  def get(host, port, path) do
+    with {:ok, client_sock} <- connect(host, port),
+         :ok <- :gen_tcp.send(client_sock, request(:get, path)),
+         {:ok, response} <- get_response(client_sock) do
       {:ok, response}
     else
       error -> error
     end
   end
 
-  def connect() do
-    :gen_tcp.connect(@host, @port, [:binary, packet: :raw, active: false])
+  def connect(host, port) do
+    :gen_tcp.connect(host, port, [:binary, packet: :raw, active: false])
   end
 
   def get_response(client_sock) do
     with {:ok, response} <- :gen_tcp.recv(client_sock, 0),
-         :ok <- :gen_tcp.close(client_sock)
-    do
+         :ok <- :gen_tcp.close(client_sock) do
       {:ok, response}
     else
       error -> error
     end
+  end
+
+  def request(:get, path) do
+    """
+    GET #{path} HTTP/1.1\r
+    Host: example.com\r
+    User-Agent: ExampleBrowser/1.0\r
+    Accept: */*\r
+    \r
+    """
   end
 end
