53aefe2a4808135df9901a9929f465805521a3d9
Adam Okoń <adam.okon@elegantcode.pl>
2020-02-13 22:19:35 +0100

    servy: add PledgeServer for creating / fetching the pledges

--- servy/lib/servy/pledge_controller.ex
+++ servy/lib/servy/pledge_controller.ex
@@ -1,23 +1,17 @@
 defmodule Servy.PledgeController do
+  alias Servy.PledgeServer
+
   def create(conv, %{"name" => name, "amount" => amount}) do
     # Sends the pledge to the external service and caches it
-    create_pledge(name, String.to_integer(amount))
+    PledgeServer.create_pledge(name, String.to_integer(amount))
 
     %{ conv | status: 201, resp_body: "#{name} pledged #{amount}!" }
   end
 
   def index(conv) do
     # Gets the recent pledges from the cache
-    pledges = recent_pledges()
+    pledges = PledgeServer.recent_pledges()
 
     %{ conv | status: 200, resp_body: (inspect pledges) }
-  end
-
-  defp create_pledge(_name, _amount) do
-
-  end
-
-  defp recent_pledges do
-
   end
 end
---
+++ servy/lib/servy/pledge_server.ex
@@ -0,0 +1,44 @@
+defmodule Servy.PledgeServer do
+  @name __MODULE__
+
+  def start(initial_state \\ []) do
+    IO.puts "Starting the pledge server..."
+    pid = spawn(__MODULE__, :listen_loop, [initial_state])
+    Process.register(pid, @name)
+    pid
+  end
+
+  def listen_loop(state) do
+    receive do
+      {:create_pledge, name, amount} ->
+        state = [{name, amount} | state]
+        listen_loop(state)
+      {sender, :recent_pledges} ->
+        send(sender, {:response, Enum.take(state, 3)})
+        listen_loop(state)
+    end
+  end
+
+  def recent_pledges do
+    send(@name, {self(), :recent_pledges})
+    receive do
+      {:response, recent_pledges} -> recent_pledges
+    end
+  end
+
+  def create_pledge(name, amount) do
+    send(@name, {:create_pledge, name, amount})
+  end
+end
+
+#pid = Servy.PledgeServer.start()
+
+#send pid, {:create_pledge, "larry", 10}
+#send pid, {:create_pledge, "moe", 20}
+#send pid, {:create_pledge, "curly", 30}
+#send pid, {:create_pledge, "daisy", 40}
+#send pid, {:create_pledge, "grace", 50}
+
+#send pid, {self(), :recent_pledges}
+
+#receive do {:response, pledges} -> IO.inspect pledges end
