82897a9133501c7481b90485ee5a97bca946ff4e
Adam Okoń <adam.okon@elegantcode.pl>
2020-04-12 15:42:06 +0200

    servy: add handling changing interval for scheduler via client API

--- servy/lib/servy/sensor_server.ex
+++ servy/lib/servy/sensor_server.ex
@@ -1,35 +1,48 @@
 defmodule Servy.SensorServer do
+  defmodule State do
+    defstruct scheduler_interval: :timer.seconds(10),
+      sensor_data: %{}
+  end
 
   @name :sensor_server
-  @scheduler_interval :timer.seconds(10)
 
   use GenServer
 
   # Client Interface
 
   def start do
-    GenServer.start(__MODULE__, %{}, name: @name)
+    GenServer.start(__MODULE__, %State{}, name: @name)
+  end
+
+  def set_refresh_interval(interval_in_ms) do
+    GenServer.cast(@name, {:set_refresh_interval, interval_in_ms})
   end
 
   def get_sensor_data do
-    GenServer.call @name, :get_sensor_data
+    GenServer.call(@name, :get_sensor_data)
   end
 
   # Server Callbacks
 
   def init(_state) do
-    initial_state = run_tasks_to_get_sensor_data()
-    schedule_refresh()
+    initial_state = %State{sensor_data: run_tasks_to_get_sensor_data()}
+    schedule_refresh(initial_state.scheduler_interval)
     {:ok, initial_state}
   end
 
-  def handle_call(:get_sensor_data, _from, state) do
-    {:reply, state, state}
+  def handle_cast({:set_refresh_interval, interval_in_ms}, state) do
+    new_state = %State{state | scheduler_interval: interval_in_ms}
+    schedule_refresh(new_state.scheduler_interval)
+    {:noreply, new_state}
   end
 
-  def handle_info(:refresh, _state) do
-    new_state = run_tasks_to_get_sensor_data()
-    schedule_refresh()
+  def handle_call(:get_sensor_data, _from, state) do
+    {:reply, state.sensor_data, state}
+  end
+
+  def handle_info(:refresh, state) do
+    new_state = %State{state | sensor_data: run_tasks_to_get_sensor_data()}
+    schedule_refresh(new_state.scheduler_interval)
     {:noreply, new_state}
   end
 
@@ -41,8 +54,8 @@ defmodule Servy.SensorServer do
     {:noreply, state}
   end
 
-  defp schedule_refresh() do
-    Process.send_after(self(), :refresh, @scheduler_interval)
+  defp schedule_refresh(interval) do
+    Process.send_after(self(), :refresh, interval)
   end
 
   defp run_tasks_to_get_sensor_data do
