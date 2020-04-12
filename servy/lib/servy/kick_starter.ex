448fa71cff60d079f5521c429212938c10973933
Adam Okoń <adam.okon@elegantcode.pl>
2020-04-12 22:26:05 +0200

    servy: add top level supervisor

--- servy/lib/servy/kick_starter.ex
+++ servy/lib/servy/kick_starter.ex
@@ -1,19 +1,23 @@
 defmodule Servy.KickStarter do
   use GenServer
 
-  def start do
+  # Client API
+
+  def get_server do
+    GenServer.call(__MODULE__, :get_server)
+  end
+
+  # Server API
+
+  def start_link(_args) do
     IO.puts "Starting the KickStarter..."
-    GenServer.start(__MODULE__, :ok, name: __MODULE__)
+    GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
   end
 
   def init(:ok) do
     Process.flag(:trap_exit, true)
     server_pid = start_http_server()
     {:ok, server_pid}
-  end
-
-  def get_server do
-    GenServer.call(__MODULE__, :get_server)
   end
 
   def handle_call(:get_server, _from, state) do
--- servy/lib/servy/supervisor.ex
+++ servy/lib/servy/supervisor.ex
@@ -1,14 +1,14 @@
-defmodule Servy.ServicesSupervisor do
+defmodule Servy.Supervisor do
   use Supervisor
 
-  def start_link(_args) do
+  def start_link() do
     Supervisor.start_link(__MODULE__, :ok, name: __MODULE__)
   end
 
   def init(:ok) do
     children = [
-      Servy.SensorServer,
-      Servy.PledgeServer
+      Servy.KickStarter,
+      Servy.ServicesSupervisor
     ]
 
     Supervisor.init(children, strategy: :one_for_one)
